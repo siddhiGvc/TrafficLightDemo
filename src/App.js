442993aaf5e8992d72c33e31907c9ee47ae01a23
siddhiGvc <siddhi@gvc.in>
2024-07-31 17:53:52 +0530

    every other shuld remain alwas red

--- src/App.js
+++ src/App.js
@@ -24,59 +24,89 @@ const TrafficLight = () => {
       
     }, [activeLight1,activeLight2,activeLight3,activeLight4]);
 
+    const handleClick1=(value)=>{
+       setActiveLight1(value);
+       setActiveLight2('R');
+       setActiveLight3('R');
+       setActiveLight4('R');
+
+    }
+
+    const handleClick2=(value)=>{
+      setActiveLight2(value);
+      setActiveLight1('R');
+      setActiveLight3('R');
+      setActiveLight4('R');
+
+   }
+   const handleClick3=(value)=>{
+    setActiveLight3(value);
+    setActiveLight1('R');
+    setActiveLight2('R');
+    setActiveLight4('R');
+
+ }
+ const handleClick4=(value)=>{
+  setActiveLight4(value);
+  setActiveLight1('R');
+  setActiveLight2('R');
+  setActiveLight3('R');
+
+}
+
   return (
     <>
     <div style={{width:'90%',display:'flex',flexWrap:'wrap',justifyContent:"space-between"}}>
        <div style={{display:"flex"}}>
             <div className="traffic-light">
-            <div onClick={()=>setActiveLight1("R")} className={`light R ${activeLight1 === 'R' ? 'active' : ''}`}></div>
-            <div onClick={()=>setActiveLight1("A")} className={`light A ${activeLight1 === 'A' ? 'active' : ''}`}></div>
-            <div onClick={()=>setActiveLight1("G")} className={`light G ${activeLight1 === 'G' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick1("R")} className={`light R ${activeLight1 === 'R' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick1("A")} className={`light A ${activeLight1 === 'A' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick1("G")} className={`light G ${activeLight1 === 'G' ? 'active' : ''}`}></div>
      
             </div>
             <div className="traffic-light">
             <div style={{visibility:'hidden',backgroundColor:"white"}} className={`light G ${activeLight1 === 'G' ? 'active' : ''}`}></div>
             <div style={{visibility:'hidden',backgroundColor:'white'}}  className={`light G ${activeLight1 === 'G' ? 'active' : ''}`}></div>
-            <div onClick={()=>setActiveLight1("G")} className={`light G ${activeLight1 === 'G' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick1("G")} className={`light G ${activeLight1 === 'G' ? 'active' : ''}`}></div>
           </div>
        </div>
        <div style={{display:"flex"}}>
             <div className="traffic-light">
-            <div onClick={()=>setActiveLight2("R")} className={`light R ${activeLight2 === 'R' ? 'active' : ''}`}></div>
-            <div onClick={()=>setActiveLight2("A")} className={`light A ${activeLight2 === 'A' ? 'active' : ''}`}></div>
-            <div onClick={()=>setActiveLight2("G")} className={`light G ${activeLight2 === 'G' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick2("R")} className={`light R ${activeLight2 === 'R' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick2("A")} className={`light A ${activeLight2 === 'A' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick2("G")} className={`light G ${activeLight2 === 'G' ? 'active' : ''}`}></div>
      
             </div>
             <div className="traffic-light">
             <div style={{visibility:'hidden',backgroundColor:"white"}} className={`light G ${activeLight2 === 'G' ? 'active' : ''}`}></div>
             <div style={{visibility:'hidden',backgroundColor:'white'}}  className={`light G ${activeLight2 === 'G' ? 'active' : ''}`}></div>
-            <div onClick={()=>setActiveLight2("G")} className={`light G ${activeLight2 === 'G' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick2("G")} className={`light G ${activeLight2 === 'G' ? 'active' : ''}`}></div>
           </div>
        </div>
        <div style={{display:"flex"}}>
             <div className="traffic-light">
-            <div onClick={()=>setActiveLight3("R")} className={`light R ${activeLight3 === 'R' ? 'active' : ''}`}></div>
-            <div onClick={()=>setActiveLight3("A")} className={`light A ${activeLight3 === 'A' ? 'active' : ''}`}></div>
-            <div onClick={()=>setActiveLight3("G")} className={`light G ${activeLight3 === 'G' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick3("R")} className={`light R ${activeLight3 === 'R' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick3("A")} className={`light A ${activeLight3 === 'A' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick3("G")} className={`light G ${activeLight3 === 'G' ? 'active' : ''}`}></div>
      
             </div>
             <div className="traffic-light">
             <div style={{visibility:'hidden',backgroundColor:"white"}} className={`light G ${activeLight3 === 'G' ? 'active' : ''}`}></div>
             <div style={{visibility:'hidden',backgroundColor:'white'}}  className={`light G ${activeLight3 === 'G' ? 'active' : ''}`}></div>
-            <div onClick={()=>setActiveLight3("G")} className={`light G ${activeLight3 === 'G' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick3("G")} className={`light G ${activeLight3 === 'G' ? 'active' : ''}`}></div>
           </div>
        </div>
        <div style={{display:"flex"}}>
             <div className="traffic-light">
-            <div onClick={()=>setActiveLight4("R")} className={`light R ${activeLight4 === 'R' ? 'active' : ''}`}></div>
-            <div onClick={()=>setActiveLight4("A")} className={`light A ${activeLight4 === 'A' ? 'active' : ''}`}></div>
-            <div onClick={()=>setActiveLight4("G")} className={`light G ${activeLight4 === 'G' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick4("R")} className={`light R ${activeLight4 === 'R' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick4("A")} className={`light A ${activeLight4 === 'A' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick4("G")} className={`light G ${activeLight4 === 'G' ? 'active' : ''}`}></div>
      
             </div>
             <div className="traffic-light">
             <div style={{visibility:'hidden',backgroundColor:"white"}} className={`light G ${activeLight4 === 'G' ? 'active' : ''}`}></div>
             <div style={{visibility:'hidden',backgroundColor:'white'}}  className={`light G ${activeLight4 === 'G' ? 'active' : ''}`}></div>
-            <div onClick={()=>setActiveLight4("G")} className={`light G ${activeLight4 === 'G' ? 'active' : ''}`}></div>
+            <div onClick={()=>handleClick4("G")} className={`light G ${activeLight4 === 'G' ? 'active' : ''}`}></div>
           </div>
        </div>
     
